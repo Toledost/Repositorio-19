d4900d08110739bde4fa2ec8b44efab0a116296e
Bruno Daniel Toledo Cadario <brunito_toledo@hotmail.com>
2020-08-08 16:16:35 -0300

    no cambie mucho

--- WFtest1920/WFtest1920/Form1.Designer.cs
+++ WFtest1920/WFtest1920/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Snow;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(11, 78);
+            this.button1.Location = new System.Drawing.Point(-3, 68);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(535, 172);
